470c10dddb754a11f880c5be25b1a3acee116076
madelen23 <brimadevillaca@gmail.com>
2025-05-26 18:22:45 -0400

    adding dockerfilev5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>HOLA DATA CENTER</code> and save to reload.
+          hdt <code>HOLA DATA CENTER</code> and save to reload.
         </p>
         <a
           className="App-link"
